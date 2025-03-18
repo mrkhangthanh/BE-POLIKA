810f68c6088a8d214f1feb10254914efaedf20f7
mrkhangthanh <mrthanhgiangkhang@gmail.com>
2025-03-18 20:59:58 +0700

    setup cors

--- src/apps/app.js
+++ src/apps/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const config = require('config');
 
+app.use(cors({
+    origin: 'http://localhost:3000', // Chỉ cho phép origin từ localhost:3000
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE','OPTIONS'], // Các phương thức được phép
+    allowedHeaders: ['Content-Type', 'Authorization'], // Các header được phép
+  }));
+
+
 app.use(express.json());
+
 app.use(`${config.get('app.prefixApiVersion')}`,require(`${__dirname}/../routers/web`));
 
 
